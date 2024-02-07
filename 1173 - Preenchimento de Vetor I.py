59f92550904335c6345571fab7b89b99c67e2797
yuri-oliver08 <104943908+yuri-oliver08@users.noreply.github.com>
2024-02-06 21:43:23 -0300

    desafios

---
+++ 1173 - Preenchimento de Vetor I.py
@@ -0,0 +1,20 @@
+'''
+Leia um valor e faça um programa que coloque o valor lido na primeira posição de um vetor N[10]. Em cada posição subsequente, coloque o dobro do valor da posição anterior. Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1,2,4,8 e assim sucessivamente. Mostre o vetor em seguida.
+
+Entrada
+A entrada contém um valor inteiro (V<=50).
+
+Saída
+Para cada posição do vetor, escreva "N[i] = X", onde i é a posição do vetor e X é o valor armazenado na posição i. O primeiro número do vetor N (N[0]) irá receber o valor de V.
+
+'''
+
+num = int(input())
+lista = [num]
+
+for x in range(10):
+    num*=2
+    lista.append(num)
+
+for i in range(len(lista)):
+    print(f'X[{i}] = {lista[i]}')
